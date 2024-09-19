84467013c46dac2466264b0808d208bf8652b837
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2024-09-19 20:35:42 +0530

    Login and signup routes completed

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,3 +1,31 @@
-export async function GET(request){
-    return new Response('Login, Next.js!')
+import { NextResponse } from "next/server";
+import { connect } from "getstream";
+import bcrypt from "bcrypt";
+import { StreamChat } from "stream-chat";
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
+
+
+export async function POST(req){
+    try{
+        const { userName, password} = req.json();
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
+        const {users} = await client.queryUsers({name: userName});
+        if(!users) return NextResponse.json({message:'User not found'},{status: 400});
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+        const token = serverClient.createUserToken(users[0].id)
+
+        if (success) {
+            return NextResponse.json({ token, fullName: users[0].fullName, username, userId: users[0].id }, { status: 200 });
+        }else{
+            return NextResponse.json({ message: 'Incorrect password' }, { status: 400 });
+        }
+    }catch(e){
+        console.log(e);
+        return NextResponse.json({message: e},{status: 500});
+    }
 }
--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,5 +1,22 @@
 import { NextResponse } from "next/server";
+import { connect } from "getstream";
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
-export async function GET(request){
-    return NextResponse.json({response: "Hello from NEXT.js"},{status: 400});
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_APP_ID;
+
+export async function POST(req){
+    try{
+        const {fullName, userName, phoneNumber, avatarURL, password, conformPassword} = req.json();
+        const userId = crypto.randomBytes(16).toString('hex');
+        const serverClient = connect(api_key, api_secret, app_id);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const token = serverClient.createUserToken(userId);
+        return NextResponse.json({ token, fullName, userName, userId, hashedPassword, phoneNumber }, { status: 200 });
+    }catch(e){
+        console.log(e);
+        return NextResponse.json({response: e},{status: 500});
+    }
 }
