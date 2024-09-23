bbe79780e9d7dbbcca9494364b8dfec1711c225f
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2024-09-23 11:55:36 +0530

    channel cntaine rcompleted message sending in group chat completed channel inner completed

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,12 @@ import { useEffect, useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
+import 'stream-chat-react/dist/css/index.css';
+//the above css is imported for default stream chat ui
 import "./page.css";
 import { ChannelListContainer, ChannelContainer } from "./components";
+
+// dynamic is imported and used for dynamic import of auth components to solve dehydration error
 import dynamic from "next/dynamic";
 const Auth = dynamic(() => import("./components/Auth"), { ssr: false });
 
@@ -14,7 +18,14 @@ const apiKey = "fqz853mkdrjr";
 function App() {
   const [client, setClient] = useState(null);
   const [authToken, setAuthToken] = useState(null);
+  // the above 2 useState client and authToken is used to solve Dehydration error 
 
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+
+  // useEffect and useState client and authToken is used to solve Dehydration error 
   useEffect(() => {
     const token = cookies.get("token");
 
@@ -38,16 +49,25 @@ function App() {
     }
   }, []);
 
+
+
   if (!authToken) return <Auth />;
   if (!client) return <div>Loading...</div>;
   return (
     <div className="app__wrapper" theme="theme light">
       <Chat client={client}>
         <ChannelListContainer 
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setIsEditing={setIsEditing}
+          setCreateType={setCreateType}
         />
         <ChannelContainer 
-          
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setIsEditing={setIsEditing}
+          isEditing={isEditing}
+          createType={createType}
         />
       </Chat>
     </div>
